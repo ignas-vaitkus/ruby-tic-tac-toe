ba1be1fcac5596a16ab24b33d3f39d2281d63ab7
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2024-10-25 17:47:25 +0300

    game start and test

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 require_relative 'space'
+require_relative 'turn'
 
 # The board of the game
 class Game
-  attr_accessor :grid, :turn
+  attr_accessor :grid, :turn, :player1, :player2, :turn_player, :game_ended
 
   def initialize
     @grid = Array.new(9).map.with_index(1) { |_, i| Space.new(i) }
+    @player1 = { name: nil, number: 1 }
+    @player2 = { name: nil, number: 2 }
+    @turn_player = player1
   end
 
   def to_s
@@ -20,5 +24,25 @@ __________
 
   def print
     puts self
+  end
+
+  def player_input(player)
+    puts "\nPlease enter a name for Player #{player[:number]}\n"
+    player[:name] = gets.chomp
+  end
+
+  def game_start
+    puts "\nWelcome to Tic Tac Toe\n\n"
+    player_input(player1)
+    player_input(player2)
+    puts "\n#{player1[:name]} will be X and #{player2[:name]} will be O\n\n"
+    puts "#{player1[:name]} goes first\n\n"
+    self.print
+  end
+
+  def play
+    game_start
+    # Turn.new(self, @turn_player).start
+    # grid[input.to_i - 1].x_or_o = 'X'
   end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -1,15 +1,28 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'stringio'
 require_relative '../lib/game'
 
 # Test app
 class GameTest < Minitest::Test
+  def setup
+    @game = Game.new
+  end
+
   def test_empty_board_print
     assert_equal '1 | 2 | 3
 __________
 4 | 5 | 6
 __________
-7 | 8 | 9', Game.new.to_s
+7 | 8 | 9', @game.to_s
+  end
+
+  def test_game_start
+    $stdin = StringIO.new("Player 1\nPlayer 2\n")
+    @game.game_start
+
+    assert_equal 'Player 1', @game.player1[:name]
+    assert_equal 'Player 2', @game.player2[:name]
   end
 end
