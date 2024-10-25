bb968cd0671cb1d4e07e3cb094020daa1e39963e
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2024-10-25 15:59:25 +0300

    Space class test

---
+++ test/space_test.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+require_relative '../lib/space'
+
+class SpaceTest < Minitest::Test
+  def setup
+    @space = Space.new(1)
+  end
+
+  def test_initialization
+    assert_nil @space.x_or_o
+    assert_equal 1, @space.number
+  end
+
+  def test_to_s_returns_number_when_x_or_o_is_nil
+    assert_equal '1', @space.to_s
+  end
+
+  def test_to_s_returns_x_or_o_when_not_nil
+    @space.x_or_o = 'X'
+
+    assert_equal 'X', @space.to_s
+  end
+
+  def test_x_or_o_accessor
+    @space.x_or_o = 'O'
+
+    assert_equal 'O', @space.to_s
+  end
+
+  def test_number_accessor
+    @space.number = 2
+
+    assert_equal 2, @space.number
+  end
+end
